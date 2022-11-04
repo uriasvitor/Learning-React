14086a853a7ff64cdb532be2b3c9c045116f5655
uriasvitor <uriasvitor@gmail.com>
2022-11-03 22:36:37 -0300

    added Localstorage

--- Curse/React-curso-01/src/App.js
+++ Curse/React-curso-01/src/App.js
@@ -1,20 +1,54 @@
-import { useState } from "react";
-import Notas from "./components/Notas";
+import { useEffect, useState } from 'react'
+import './App.css'
 
 function App(){
-  const [aluno, setAluno] = useState('Nome do cara')
+  const [listas, setLista] = useState([])
+  const [input, setInput] = useState('')
+  
+  useEffect(()=>{
+    const tarefasStorage = localStorage.getItem('@listas');
 
-  function changeName(nome){
-    setAluno(nome)
+    if(tarefasStorage){
+      setLista(JSON.parse(tarefasStorage))
+      console.log(tarefasStorage)
+    }
+
+  },[])
+
+
+  function addList(e){
+    e.preventDefault()
+    setLista([...listas,input])
+    localStorage.setItem('@listas', JSON.stringify(listas))
   }
 
   return(
     <div>
-      <h1>Aluno: {aluno} </h1>
-      <Notas data="12/12/2012"/>
-      <button onClick={()=> changeName('matheus')}></button>
+      <div className='container'>
+          <h1>Welcome to real world</h1>
+
+          <form onSubmit={ addList}>
+              <label>Lista</label>
+              <br></br>
+              <input 
+                  id="lista" 
+                  placeholder='Adicione a lista'
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+              ></input>
+
+            <button>Hello world</button>
+
+          </form>
+
+          <div>
+            {listas.map( lists =>(
+              <li key={lists}>{lists}</li>
+            ))}
+          </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
